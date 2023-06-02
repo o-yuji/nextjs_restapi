cc464d67609d8ffa6a0abe341585a5b76a628555
o-yuji <py_ootsuka@yahoo.co.jp>
2023-06-03 00:07:09 +0900

    async delete

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='local_secret_here')
 
-DEBUG = 'RENDER' not in os.environ
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
