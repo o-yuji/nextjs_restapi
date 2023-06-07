8c05798f19e118599d0fc7e16dcfa122d06b30bf
o-yuji <py_ootsuka@yahoo.co.jp>
2023-06-07 21:33:17 +0900

    task

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TaskListView,TaskRetrieveView,TaskViewSet,PostListView,PostRetrieveView,CreateUserView
 
 router = DefaultRouter()
-router.register('tasks',TaskViewSet,basename='tasks')
+router.register('tasks', TaskViewSet, basename='tasks')
 
 urlpatterns = [
     path('list-post/', PostListView.as_view(), name='list-post'),
